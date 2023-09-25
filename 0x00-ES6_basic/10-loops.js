47eaadc48e85236ca082ca2b950ef48650d1bec0
Chebet Kamara <chebetkamara@gmail.com>
2023-09-25 12:07:57 +0000

    Fixing errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const value = array.indexOf(idx);
     array[value] = appendString + idx;
-  },
+  }
 
   return array;
 }
