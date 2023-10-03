aeadd3ba90fcffd8c1fd5d3152687fd9f5a3cc15
Chebet Kamara <chebetkamara@gmail.com>
2023-10-03 15:49:21 +0000

    Fixing linter errors

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,21 @@
 export default function getListStudents() {
-  const a  = {
+  const a = {
     id: 1,
     firstName: 'Guillaume',
     location: 'San Francisco',
-  }
+  };
 
   const b = {
     id: 2,
     firstName: 'James',
     location: 'Columbia',
-  }
+  };
 
   const c = {
     id: 5,
     firstName: 'Serena',
     location: 'San Francisco',
-  }
+  };
 
   return [a, b, c]
 }
