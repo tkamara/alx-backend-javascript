b4a332e0a4203c0a6e3f3f9743a999812700a723
Chebet Kamara <chebetkamara@gmail.com>
2023-11-21 13:01:09 +0000

    NodeJS basics

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+console.log('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+});
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit();
+  });
+
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+    process.exit(0);
+});
+}
