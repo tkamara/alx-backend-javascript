0ed363c39e7135df3bb9a75821422cc85f6a50ea
Chebet Kamara <chebetkamara@gmail.com>
2023-11-21 17:14:00 +0000

    Fixing errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,18 @@
-console.log('Welcome to Holberton School, what is your name?\n');
-
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf-8');
 if (process.stdin.isTTY) {
-  process.stdin.once('data', (data) => {
+  process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data}`);
     process.exit();
   });
 } else {
-  process.stdin.once('data', (data) => {
+  process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data}`);
     process.exit();
   });
-}
 
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
-    process.exit(0);
+    process.exit();
   });
+}
