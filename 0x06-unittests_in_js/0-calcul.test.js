b896ce325c477e18f8252f47e6d2f61239445fe5
Chebet Kamara <chebetkamara@gmail.com>
2023-11-22 18:30:44 +0000

    Unittests

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () =>
+  it('should return rounded sum', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1.6, 3), 5);
+    assert.strictEqual(calculateNumber(2, 4.1), 6);
+    assert.strictEqual(calculateNumber(2.3, 4.8), 7);
+  });
+
+  it('should handle negative numbers', () => {
+    assert.strictEqual(calculateNumber(1, -3), -2);
+    assert.strictEqual(calculateNumber(-1.6, 3), 1);
+    assert.strictEqual(calculateNumber(-2, 4.1), 2);
+    assert.strictEqual(calculateNumber(2.3, -4.8), -3);
+  });
+
+  it('should handle non-numbers', () => {
+    assert.throws(() => {
+      calculateNumber(NaN, 2);
+    }, TypeError);
+  });
---
+++ 0x06-unittests_in_js/0-main.js
@@ -0,0 +1,3 @@
+const calculateNumber = require("./0-calcul.js");
+console.log(calculateNumber(1, 3));
+console.log(calculateNumber(1, 3.7));
